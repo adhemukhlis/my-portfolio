66ad65b172eb44a9da00e20d27c70f27051eebed
adhemukhlis <adhemukhlis@gmail.com>
2020-04-13 01:01:48 +0700

    13042020-2

--- src/portfolio/intro_prop.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { $img, $ImgContainer } from "./style";
-export const ProfileImg = props => (
-	<div style={$ImgContainer}><div style={{
-		...$img,
-		backgroundImage: "url(" + props.url + ")"
-	}} alt="img_profile"/></div>
-);
